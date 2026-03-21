6964eb8bc70f6d467e24fce4b49a83179042faf0
amenouveveLight <amenouvevelight@gmail.com>
2026-03-21 07:46:48 +0000

    Correction erreur UrlGenerator et force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\UrlGenerator; 
 
 
 
